f6d07318d739330279e99be2c5c47d09421e5075
vinspoint-ALX <chikezie vincent>
2022-09-09 09:55:47 -0700

    1 task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- *  * main - Entry point
+ *  * main - print quote
  *   *
- *    * Description: prints "with proper grammar, but the outcome is a piece of
- *     * art,"
- *      *
- *       * Return: Always 0 (Success)
- *        */
+ *    * Description: prints Dora's quote
+ *     *
+ *      * Return: Always 1 (Success)
+ *       */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		        return (1);
 }
