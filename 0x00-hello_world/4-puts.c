940f117f40a7864dd51cc5fb7fc4f913c8a70a85
vinspoint-ALX <chikezie vincent>
2022-09-09 13:53:58 -0700

    5 task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- * 
- * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	
+		return (0);
+
 }
