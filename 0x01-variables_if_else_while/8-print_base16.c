98382e3ebc1a88e6c1c409c4a84c41c23c283e8d
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2022-09-10 02:17:21 +0100

    7=advance

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,26 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
- *  * main - print the letters of the alphabet
- *   *
- *     * Description: print the letters of the alphabet
- *       *
- *        * Return: Always 0 (Success)
- *         */
+ * main - print the letters of the alphabet
+ *
+  * Description: print the letters of the alphabet
+  *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int i = 0;
+	int i = 0;
 
-			while (i < 48)
-					{
-								if (i < 10)
-												putchar(i + '0');
-										else if (i > 41)
-														putchar(i - 10 + 'A');
-												i++;
-													}
-				putchar(10);
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+	putchar(10);
 
-				return (0);
+return (0);
 }
