2aae86c47cd5e26791eba73eb24d29901712779f
vinspoint-ALX <chikezie vincent>
2022-09-09 17:44:51 -0700

    7 task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+
+/**
+ *  * main - print the letters of the alphabet
+ *   *
+ *     * Description: print the letters of the alphabet
+ *       *
+ *        * Return: Always 0 (Success)
+ *         */
+
+int main(void)
+{
+		int i = 122;
+
+			while (i > 96)
+					{
+								putchar(i);
+										i--;
+											}
+				putchar(10);
+
+				return (0);
+}
