eaf5402e463dc7708450483760915e439274ab63
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2022-09-10 01:19:21 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
- *  * main - print quote
- *   *
- *    * Description: prints Dora's quote
- *     *
- *      * Return: Always 1 (Success)
- *       */
-
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		        return (1);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
