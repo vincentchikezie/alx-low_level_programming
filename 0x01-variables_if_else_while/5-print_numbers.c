67132b59bd22c3877daf9dbc19087b766d138e6b
EMMANUEL-ALX <emmanuelanaedobe@gmail.com>
2022-09-10 01:00:49 +0100

    5-task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- *  * main - print single digit numbers
- *   *
- *    * Description: print single digit numbers
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int i;
+	int i;
 
-		i = 48;
+i = 48;
 
-			while (i < 58)
-					{
-								putchar(i);
-										i++;
-											}
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
 
-			putchar(10);
+putchar(10);
 
-				return (0);
+	return (0);
 }
