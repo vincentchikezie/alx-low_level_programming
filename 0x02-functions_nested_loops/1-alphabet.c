96f8654fd4611245dceba4130aef323d4e109254
vinspoint-ALX <chikezie vincent>
2022-09-13 11:59:26 -0700

    1-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *  
+ *  Return: 0 on success always
+ */
+
+int main(void)
+{
+	char vin[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(vin[i];
+	}
+	_putchar('\n');
+
+	return (0);
+}
