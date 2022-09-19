c99c37ae44137ccd90ec3140fa751803f32c159e
vinspoint-ALX <chikezie vincent>
2022-09-19 15:31:57 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+Executable File  17 lines (15 sloc)  217 Bytes
+
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of int a and int b
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
